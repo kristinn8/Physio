da0631a3ae4a19006d0fa751dbee7091a3e1dbe7
kristinn8 <kristinn8@gmail.com>
2016-03-28 22:27:18 +0000

    penis

--- app/src/main/java/com/mycompany/sport/MainActivity.java
+++ app/src/main/java/com/mycompany/sport/MainActivity.java
@@ -5,17 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.content.Intent;
-import android.widget.EditText;
+        import android.content.Intent;
+        import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+        public class MainActivity extends ActionBarActivity {
+            public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_rating);
+                //setContentView(R.layout.activity_main);
     }
 
 
---
+++ app/src/main/java/com/mycompany/sport/Rating.java
@@ -0,0 +1,42 @@
+package com.mycompany.sport;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class Rating extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        final TextView seekBarValue = (TextView)findViewById(R.id.seekBarValue);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            seekBarValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+}
